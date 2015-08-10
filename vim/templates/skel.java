599d9e496a35ea7eaa1ad6d88446d85bd0896ee6
siidney <siidney.watson@gmail.com>
2015-08-10 06:16:08 -0600

    Added basic javadoc to skel.java

--- vim/templates/skel.java
+++ vim/templates/skel.java
@@ -1,17 +1,13 @@
 /*
- * =====================================================================================
- *
  *       Filename:  [:VIM_EVAL:]expand('%:t')[:END_EVAL:]
  *
  *    Description:  
  *
- *        Version:  1.0
- *        Created:  [:VIM_EVAL:]strftime('%d/%m/%y %T')[:END_EVAL:]
+ *         reated:  [:VIM_EVAL:]strftime('%d/%m/%y %T')[:END_EVAL:]
  *       Revision:  none
- *       Compiler:  gcc
  *
- *         Author:  Siidney Watson - siidney.watson@gmail.com
- *   Organization:  LolaDog Studio
+ *        @Author:  Siidney Watson - siidney.watson@gmail.com
+ *        @Version:  1.0
  *
  * =====================================================================================
  */

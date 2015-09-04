cd164a9de52db8910f7baa19e5b61b9ff738f517
siidney <siidney.watson@gmail.com>
2015-09-04 08:47:56 -0600

    imdbsearch -  more Pythonic function naming, added docstrings.

---
+++ vim/templates/skel.ino
@@ -0,0 +1,24 @@
+/*
+        Filename:  [:VIM_EVAL:]expand('%:t')[:END_EVAL:]
+
+     Description:  
+
+     The Circuit:  input:
+                   output:
+
+         Version:  1.0
+         Created:  [:VIM_EVAL:]strftime('%d/%m/%y %T')[:END_EVAL:]
+        Modified:  none
+
+          Author:  Siidney Watson - siidney.watson@gmail.com
+*/
+
+// Initialisation
+void setup(){
+
+}
+
+// main
+void loop(){
+
+}
